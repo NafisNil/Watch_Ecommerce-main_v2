cdf8f49e99585d5e3a59a02730e5faa020dde1f7
NafisNil <nafis.zaman1.an@gmail.com>
2024-06-26 17:00:27 +0600

    cart_2

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -14,7 +14,38 @@ class CartController extends Controller
     public function add_to_cart(Request $request){
         if ($request->session()->has('cart')) {
             # code...
-            $cart = $request->session()->get('cart')
+            $cart = $request->session()->get('cart');
+            $product_array_ids = array_column($cart, 'id');
+            $id = $request->input('id');
+            if (!in_array($id, $product_array_ids )) {
+                # code...
+                $name = $request->input('name');
+                $image = $request->input('image');
+                $price = $request->input('price');
+                $quantity = $request->input('quantity');
+                $sale_price = $request->input('sale_price');
+
+                if ($sale_price != null) {
+                    # code...
+                    $price_to_charge = $sale_price;
+                }else{
+                    $price_to_charge = $price;
+                }
+
+                $product_array = array(
+                    'id' => $id,
+                    'name' => $name,
+                    'price' => $price_to_charge,
+                    'image' => $image,
+                    'quantity' => $quantity
+                );
+
+                $card[$id] = $product_array;
+                $request->session()->put('cart', $cart);
+              
+            }else{
+                echo "<script>alert('product is already in cart');</script>";
+            }
         }else{
 
         }
